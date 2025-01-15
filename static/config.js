3adfd1a60e4703448fe77787e7cf1b5a47399082
Samuni <sanghoon01@uengine.org>
2025-01-15 12:04:36 +0900

    Update config.js

--- static/config.js
+++ static/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 window._env_ = {
-  DB_URL: process.env.DB_URL,
-  DB_PW: process.env.DB_PW
+  DB_URL: "https://edictibjpvjyzelrjepa.supabase.co",
+  DB_PW: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkaWN0aWJqcHZqeXplbHJqZXBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY5MDU4OTgsImV4cCI6MjA1MjQ4MTg5OH0.GSjf7niZNfblAmMcW2WAFbYl3GQWu4XNe0tpkPiExBc"
 }
